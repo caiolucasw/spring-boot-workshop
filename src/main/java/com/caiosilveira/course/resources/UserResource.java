6ff9b1f184ae0df154535b11af10b3802b20fa16
Caio <caiolsds@gmail.com>
2024-01-02 15:19:20 -0300

    User entity and User Resource were added

---
+++ src/main/java/com/caiosilveira/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.caiosilveira.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.caiosilveira.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Caio", "caio@gmail.com", "1193333333", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
